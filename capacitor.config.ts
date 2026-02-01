4391c2ce30f696fde535c07720bae42de86d892a
maris ibrahim <64644872+manyar-su@users.noreply.github.com>
2026-02-01 10:15:49 +0700

    feat: Update app icon to BRADWEAR neon logo and app name to Bradflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.bradwear.flow',
-  appName: 'bradflow',
+  appName: 'Bradflow',
   webDir: 'dist'
 };
 
