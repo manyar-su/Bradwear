ac41b9b5c7d9228c62491e7b5aef64adae82b63c
maris ibrahim <64644872+manyar-su@users.noreply.github.com>
2026-03-30 23:00:34 +0700

    v2.7.5: add permissions camera, storage, notifications, time sync

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bradwear.flow',
   appName: 'Bradflow',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    allowMixedContent: true
+  },
+  plugins: {
+    LocalNotifications: {
+      smallIcon: 'ic_stat_icon_config_sample',
+      iconColor: '#10b981',
+      sound: 'beep.wav'
+    }
+  }
 };
 
 export default config;
